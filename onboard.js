5811b0798017282b7b1ba8e810d320fe2d5375c2
Karl August Nestor <jesuscaesar@jesuscaesar.com>
2023-10-06 20:43:22 +1000

    Updated Monopoly board

--- onboard.js
+++ onboard.js
@@ -62,54 +62,54 @@ function luxurytax() {
 }
 
 function citytax() {
-	addAlert(player[turn].name + " paid $200 for landing on City Tax.");
+	addAlert(player[turn].name + " paid $200 for landing on Income Tax.");
 	player[turn].pay(200, 0);
 
-	$("#landed").show().text("You landed on City Tax. Pay $200.");
+	$("#landed").show().text("You landed on Income Tax. Pay $200.");
 }
 
 var square = [];
 
 square[0] = new Square("Start", "COLLECT $200 SALARY AS YOU PASS.", "#FFFFFF");
-square[1] = new Square("Thessaloniki", "$60", "#8B4513", 60, 3, 2, 10, 30, 90, 160, 250);
+square[1] = new Square("Strandvejen (Denmark)", "$60", "#8B4513", 60, 3, 2, 10, 30, 90, 160, 250);
 square[2] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[3] = new Square("Athens", "$60", "#8B4513", 60, 3, 4, 20, 60, 180, 320, 450);
-square[4] = new Square("City Tax", "Pay $200", "#FFFFFF");
-square[5] = new Square("Golden Pass", "$200", "#FFFFFF", 200, 1);
-square[6] = new Square("Naples", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550);
+square[3] = new Square("Slotsgade (Denmark)", "$60", "#8B4513", 60, 3, 4, 20, 60, 180, 320, 450);
+square[4] = new Square("Income Tax", "Pay $200", "#FFFFFF");
+square[5] = new Square("Luxembourg Airport (Luxembourg)", "$200", "#FFFFFF", 200, 1);
+square[6] = new Square("Avenue Louise/Louizalaan (Belgium)", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550);
 square[7] = new Square("Chance", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[8] = new Square("Milan", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550);
-square[9] = new Square("Rome", "$120", "#87CEEB", 120, 4, 8, 40, 100, 300, 450, 600);
+square[8] = new Square("Rue des Bouchers/Beenhouwers-straat (Belgium)", "$100", "#87CEEB", 100, 4, 6, 30, 90, 270, 400, 550);
+square[9] = new Square("Grand-Place/Grote Markt (Belgium)", "$120", "#87CEEB", 120, 4, 8, 40, 100, 300, 450, 600);
 square[10] = new Square("Just Visiting", "", "#FFFFFF");
-square[11] = new Square("Barcelona", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750);
+square[11] = new Square("Coolsingel (Netherlands)", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750);
 square[12] = new Square("Electric Company", "$150", "#FFFFFF", 150, 2);
-square[13] = new Square("Sevilla", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750);
-square[14] = new Square("Madrid", "$160", "#FF0080", 160, 5, 12, 60, 180, 500, 700, 900);
-square[15] = new Square("English Channel", "$200", "#FFFFFF", 200, 1);
-square[16] = new Square("Liverpool", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950);
+square[13] = new Square("Lange Poten (Netherlands)", "$140", "#FF0080", 140, 5, 10, 50, 150, 450, 625, 750);
+square[14] = new Square("Kalverstraat (Netherlands)", "$160", "#FF0080", 160, 5, 12, 60, 180, 500, 700, 900);
+square[15] = new Square("Portugal Airport (Portugal)", "$200", "#FFFFFF", 200, 1);
+square[16] = new Square("Paseo de la Castellana (Spain)", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950);
 square[17] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[18] = new Square("Glasgow", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950);
-square[19] = new Square("London", "$200", "#FFA500", 200, 6, 16, 80, 220, 600, 800, 1000);
+square[18] = new Square("Gran Via (Spain)", "$180", "#FFA500", 180, 6, 14, 70, 200, 550, 750, 950);
+square[19] = new Square("Las Ramblas (Spain)", "$200", "#FFA500", 200, 6, 16, 80, 220, 600, 800, 1000);
 square[20] = new Square("Free Parking", "", "#FFFFFF");
-square[21] = new Square("Rotterdam", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050);
+square[21] = new Square("Oxford Street (Great Britain)", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050);
 square[22] = new Square("Chance", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[23] = new Square("Brussels", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050);
-square[24] = new Square("Paris", "$240", "#FF0000", 240, 7, 20, 100, 300, 750, 925, 1100);
-square[25] = new Square("Orient Express", "$200", "#FFFFFF", 200, 1);
-square[26] = new Square("Amsterdam", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150);
-square[27] = new Square("Göteborg", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150);
+square[23] = new Square("Piccadilly (Great Britain)", "$220", "#FF0000", 220, 7, 18, 90, 250, 700, 875, 1050);
+square[24] = new Square("Park Lane (Great Britain)", "$240", "#FF0000", 240, 7, 20, 100, 300, 750, 925, 1100);
+square[25] = new Square("Athens Airport (Greece)", "$200", "#FFFFFF", 200, 1);
+square[26] = new Square("Via Veneto (Italy)", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150);
+square[27] = new Square("Via Condotti (Italy)", "$260", "#FFFF00", 260, 8, 22, 110, 330, 800, 975, 1150);
 square[28] = new Square("Water Works", "$150", "#FFFFFF", 150, 2);
-square[29] = new Square("Stockholm", "$280", "#FFFF00", 280, 8, 24, 120, 360, 850, 1025, 1200);
+square[29] = new Square("Via Monte Napoleone (Italy)", "$280", "#FFFF00", 280, 8, 24, 120, 360, 850, 1025, 1200);
 square[30] = new Square("Go to Jail", "Go directly to Jail. Do not pass GO. Do not collect $200.", "#FFFFFF");
-square[31] = new Square("Frankfurt", "$300", "#008000", 300, 9, 26, 130, 390, 900, 1100, 1275);
-square[32] = new Square("Cologne", "$300", "#008000", 300, 9, 26, 130, 390, 900, 1100, 1275);
+square[31] = new Square("Avenue Foch (France)", "$300", "#008000", 300, 9, 26, 130, 390, 900, 1100, 1275);
+square[32] = new Square("Avenue des Champs-Élysées (France)", "$300", "#008000", 300, 9, 26, 130, 390, 900, 1100, 1275);
 square[33] = new Square("Community Chest", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[34] = new Square("Bonn", "$320", "#008000", 320, 9, 28, 150, 450, 1000, 1200, 1400);
-square[35] = new Square("Golden Pass", "$200", "#FFFFFF", 200, 1);
+square[34] = new Square("Rue de la Paix (France)", "$320", "#008000", 320, 9, 28, 150, 450, 1000, 1200, 1400);
+square[35] = new Square("Dublin Airport (Ireland)", "$200", "#FFFFFF", 200, 1);
 square[36] = new Square("Chance", "FOLLOW INSTRUCTIONS ON TOP CARD", "#FFFFFF");
-square[37] = new Square("Innsbruck", "$350", "#0000FF", 350, 10, 35, 175, 500, 1100, 1300, 1500);
+square[37] = new Square("Königsallee (Germany)", "$350", "#0000FF", 350, 10, 35, 175, 500, 1100, 1300, 1500);
 square[38] = new Square("Luxury Tax", "Pay $100", "#FFFFFF");
-square[39] = new Square("Vienna", "$400", "#0000FF", 400, 10, 50, 200, 600, 1400, 1700, 2000);
+square[39] = new Square("Kurfürstendamm (Germany)", "$400", "#0000FF", 400, 10, 50, 200, 600, 1400, 1700, 2000);
 
 var communityChestCards = [];
 var chanceCards = [];
@@ -139,12 +139,12 @@ chanceCards[3] = new Card("You have been elected chairman of the board. Pay each
 chanceCards[4] = new Card("Go back three spaces.", function() { gobackthreespaces();});
 chanceCards[5] = new Card("Advance to the nearest utility. IF unowned, you may buy it from the Bank. IF OWNED, throw dice and pay owner a total ten times the amount thrown.", function() { advanceToNearestUtility();});
 chanceCards[6] = new Card("Bank pays you dividend of $50.", function() { addamount(50, 'Chance');});
-chanceCards[7] = new Card("Advance to the nearest railroad. If unowned, you may buy it from the Bank. If OWNED, pay owner twice the rental to which they are otherwise entitled.", function() { advanceToNearestRailroad();});
+chanceCards[7] = new Card("Advance to the nearest airport. If unowned, you may buy it from the Bank. If OWNED, pay owner twice the rental to which they are otherwise entitled.", function() { advanceToNearestRailroad();});
 chanceCards[8] = new Card("Pay poor tax of $15.", function() { subtractamount(15, 'Chance');});
-chanceCards[9] = new Card("Take a trip to Reading Rail Road. If you pass \"Start\" collect $200.", function() { advance(5);});
-chanceCards[10] = new Card("Advance to Vienna.", function() { advance(39);});
-chanceCards[11] = new Card("Advance to Paris. If you pass \"Start\" collect $200.", function() { advance(24);});
+chanceCards[9] = new Card("Take a trip to Luxembourg Airport (Luxembourg). If you pass \"Start\" collect $200.", function() { advance(5);});
+chanceCards[10] = new Card("Advance to Kurfürstendamm (Germany).", function() { advance(39);});
+chanceCards[11] = new Card("Advance to Park Lane (Great Britain). If you pass \"Start\" collect $200.", function() { advance(24);});
 chanceCards[12] = new Card("Your building loan matures. Collect $150.", function() { addamount(150, 'Chance');});
-chanceCards[13] = new Card("Advance to the nearest railroad. If unowned, you may buy it from the Bank. If OWNED, pay owner twice the rental to which they are otherwise entitled.", function() { advanceToNearestRailroad();});
-chanceCards[14] = new Card("Advance to Barcelona. If you pass \"Start\" collect $200.", function() { advance(11);});
+chanceCards[13] = new Card("Advance to the nearest airport. If unowned, you may buy it from the Bank. If OWNED, pay owner twice the rental to which they are otherwise entitled.", function() { advanceToNearestRailroad();});
+chanceCards[14] = new Card("Advance to Coolsingel (Netherlands). If you pass \"Start\" collect $200.", function() { advance(11);});
 chanceCards[15] = new Card("Go to Jail. Go Directly to Jail. Do not pass \"Start\". Do not collect $200.", function() { gotojail();});
